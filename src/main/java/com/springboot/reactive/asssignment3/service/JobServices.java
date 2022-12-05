47051b01ea0cf56322031a6a95c4d36a0a7dad61
Sahoo <panchanan.sahoo@capgemini.com>
2022-12-05 20:47:43 +0530

    Initial commit

---
+++ src/main/java/com/springboot/reactive/asssignment3/service/JobServices.java
@@ -0,0 +1,59 @@
+package com.springboot.reactive.asssignment3.service;
+
+import com.springboot.reactive.asssignment3.entity.Employee;
+import com.springboot.reactive.asssignment3.entity.Job;
+import com.springboot.reactive.asssignment3.entity.JobResponse;
+import com.springboot.reactive.asssignment3.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+
+@Service
+public class JobServices {
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
+    public Mono<JobResponse> createJob(Job job) {
+        return jobRepository.existsByjobId(job.getJobId())
+                .flatMap(aBoolean -> {
+                    if(aBoolean){
+                        return Mono.just(new JobResponse(job.getJobId(),job.getJobName(),job.getJavaExp(),job.getSpringExp(),"Already Exists"));
+                    }
+                    else{
+                        return jobRepository.save(job).
+                                map(j-> new JobResponse(job.getJobId(),job.getJobName(),job.getJavaExp(),job.getSpringExp(),"Created"));
+
+                    }
+
+                        }
+                );
+    }
+
+
+    public Flux<Employee> findEmpForJobId(int jobId){
+
+        Flux<Employee> employeeFlux = jobRepository.findByjobId(jobId).flatMap(job -> {
+
+            return webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/findEmpSkillSet/" + job.getJavaExp())
+                    .retrieve()
+                    .bodyToFlux(Employee.class);
+
+
+        });
+        return employeeFlux;
+
+    }
+}
+
